c8ebe849927989ae70ad74d846084bf4a5936a2d
ShermamS <202308388525@alunos.estacio.br>
2023-10-26 22:22:22 -0400

    outros / novos arquivos

---
+++ outros/fisico.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS FSEBVU;
+DEFAULT CHARACTER SET utf8mb4;
+COLLATE utf8mb4_general_ci;
+
+USE FSEBVU;
+
+CREATE TABLE usuario(
+    id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(60) NOT NULL,
+    senha VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE receita(
+    id_receita INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    descricao VARCHAR(60) NOT NULL,
+    ingredientes VARCHAR(32) NOT NULL,
+    preparo VARCHAR(200) NOT NULL,
+    categoria VARCHAR(1) NOT NULL,
+    img_receita VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE avaliacao(
+    id_avaliacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    autor_avaliacao VARCHAR(50) NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    nota VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE favorito(
+    id_favorito INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE categoria(
+    id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sobremesas VARCHAR(1) NOT NULL,
+    pratos_principais VARCHAR(1) NOT NULL,
+    bebidas VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE comentario(
+    id_comentario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),         /* data_comentario */
+    autor_comentario VARCHAR(1) NOT NULL
+);
